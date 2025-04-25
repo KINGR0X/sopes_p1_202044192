9e5cdcabd5cb20b4550906f2f37ac588cbd28cc6
elianreyes <elianreyes121@gmail.com>
2025-04-25 01:43:01 -0600

    test no rabbit

--- Proyecto2/grpc-client/main.go
+++ Proyecto2/grpc-client/main.go
@@ -16,9 +16,8 @@ import (
 )
 
 var (
-	// conexiones con los servidores gRPC
-	addr1 = flag.String("addr1", "grpc_server_service_rabbit:50051", "the address to connect to")
-	addr2 = flag.String("addr2", "grpc_server_service_kafka:50051", "the address to connect to")
+	// conexión con el servidor gRPC
+	addr = flag.String("addr", "grpc_server_service_kafka:50051", "the address to connect to")
 )
 
 type Tweet struct {
@@ -39,77 +38,74 @@ func sendData(fiberCtx *fiber.Ctx) error {
 
 	fmt.Println("Received Tweet data:", body)
 
-	// Conexión a los servidores gRPC
-	addresses := []string{*addr1, *addr2}
+	// Conexión al servidor gRPC
+	address := *addr
 
-	// Enviar datos a los servidores gRPC usando goroutines
+	// Enviar datos al servidor gRPC
 	var wg sync.WaitGroup
-	responses := make([]*pb.TweetResponse, len(addresses))
-	errors := make([]error, len(addresses))
+	wg.Add(1)
+	
+	var response *pb.TweetResponse
+	var grpcErr error
+	
+	go func() {
+		defer wg.Done()
 
-	for i, address := range addresses {
-		wg.Add(1)
-		go func(i int, addr string) {
-			defer wg.Done()
+		conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			grpcErr = fmt.Errorf("did not connect to %s: %v", address, err)
+			return
+		}
+		defer conn.Close()
 
-			conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-			if err != nil {
-				errors[i] = fmt.Errorf("did not connect to %s: %v", addr, err)
-				return
-			}
-			defer conn.Close()
+		c := pb.NewTweetClient(conn)
 
-			c := pb.NewTweetClient(conn)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
 
-			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-			defer cancel()
+		var weather pb.Weather
+		// cambiar el valor de weather a un valor de la enumeración Weather
+		switch body.Weather {
+		case 0:
+			weather = pb.Weather_rainy
+		case 1:
+			weather = pb.Weather_cloudy
+		case 2:
+			weather = pb.Weather_sunny
+		default:
+			weather = pb.Weather_rainy
+		}
 
-			var weather pb.Weather
-			// cambiar el valor de weather a un valor de la enumeración Weather
-			switch body.Weather {
-			case 0:
-				weather = pb.Weather_rainy
-			case 1:
-				weather = pb.Weather_cloudy
-			case 2:
-				weather = pb.Weather_sunny
-			default:
-				weather = pb.Weather_rainy
-			}
+		// Enviar el mensaje al servidor gRPC
+		r, err := c.SendTweet(ctx, &pb.TweetRequest{ 
+			Description: body.Descripcion,
+			Country:     body.Country,
+			Weather:     weather,
+		})
 
-			// Enviar el mensaje al servidor gRPC
-			r, err := c.SendTweet(ctx, &pb.TweetRequest{ 
-				Description: body.Descripcion,
-				Country:     body.Country,
-				Weather:     weather,
-			})
+		// Si hay un error
+		if err != nil {
+			grpcErr = fmt.Errorf("error from %s: %v", address, err)
+			return
+		}
 
-			// Si hay un error
-			if err != nil {
-				errors[i] = fmt.Errorf("error from %s: %v", addr, err)
-				return
-			}
-
-			responses[i] = r
-			fmt.Printf("Received response from gRPC server %s: %v\n", addr, r)
-		}(i, address)
-	}
+		response = r
+		fmt.Printf("Received response from gRPC server %s: %v\n", address, r)
+	}()
 
 	wg.Wait()
 
-	// si hubieron errores devuelve error 500
-	for _, err := range errors {
-		if err != nil {
-			return fiberCtx.Status(500).JSON(fiber.Map{
-				"error": err.Error(),
-			})
-		}
+	if grpcErr != nil {
+		return fiberCtx.Status(500).JSON(fiber.Map{
+			"error": grpcErr.Error(),
+		})
 	}
 
 	// ver si el mensaje llego bien
 	return fiberCtx.JSON(fiber.Map{
-		"success": true,
-		"message": "Data sent to all servers successfully",
+		"success":  true,
+		"message":  "Data sent to server successfully",
+		"response": response, // Ahora se usa la variable response
 	})
 }
 
